9cdbb09743425dfdac55ce1f85080f809bf417e0
Amir Azmi <amir.azmi@prodigious.com>
2023-07-12 20:43:44 +0800

    add basic docs template

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,6 +1,5 @@
 export const collectLabels = (posts) => {
   const labels: string[] = [];
-  console.log("masuk sini ke");
   posts.forEach(({ frontmatter }) => {
     const postLabels = frontmatter.label.split(", ");
     postLabels.forEach((label) => {
