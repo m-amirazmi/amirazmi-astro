f416787da1e2dec93e81b7e0cbf209767aa2006f
Amir Azmi <amir.azmi@prodigious.com>
2023-07-17 17:11:33 +0800

    add mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from "astro/config";
 import robotsTxt from "astro-robots-txt";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://amirazmi.dev",
-  integrations: [robotsTxt()],
+  integrations: [robotsTxt(), mdx()]
 });
