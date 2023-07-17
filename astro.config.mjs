58015362d9985d26df20a699a6934eb4b7c5a931
Muhamad Amir <m.amirazmi@hotmail.com>
2023-07-17 22:02:41 +0800

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
 import robotsTxt from "astro-robots-txt";
 import mdx from "@astrojs/mdx";
-
 import react from "@astrojs/react";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://amirazmi.dev",
-  integrations: [robotsTxt(), mdx(), react()]
+  integrations: [robotsTxt(), mdx(), react(), sitemap()]
 });
