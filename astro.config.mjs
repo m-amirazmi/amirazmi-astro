206e39b316d9a3aedec498284fcda7c2129fbb4f
Amir Azmi <amir.azmi@prodigious.com>
2023-07-17 18:33:12 +0800

    add some article content

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
 import robotsTxt from "astro-robots-txt";
-
 import mdx from "@astrojs/mdx";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://amirazmi.dev",
-  integrations: [robotsTxt(), mdx()]
+  integrations: [robotsTxt(), mdx(), react()]
 });
