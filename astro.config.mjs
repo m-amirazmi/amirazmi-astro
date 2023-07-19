aa2eab8f0e1ec00772bf77573c716527ce49e1b2
Muhamad Amir <m.amirazmi@hotmail.com>
2023-07-19 13:26:20 +0800

    add partytown and remove animation for navbar for non homepage

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,15 @@ import { defineConfig } from "astro/config";
 import robotsTxt from "astro-robots-txt";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
-
 import sitemap from "@astrojs/sitemap";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   experimental: {
-    assets: true,
+    assets: true
   },
   site: "https://amirazmi.dev",
-  integrations: [robotsTxt(), mdx(), react(), sitemap()],
+  integrations: [robotsTxt(), mdx(), react(), sitemap(), partytown()]
 });
