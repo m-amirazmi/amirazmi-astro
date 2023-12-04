117ba08d7723ffd5faebe4698eeac047295899f3
Muhamad Amir <m.amirazmi@hotmail.com>
2023-12-04 18:41:11 +0800

    add meta tags

--- src/components/base-head.tsx
+++ src/components/base-head.tsx
@@ -6,12 +6,16 @@ interface IProps {
   title: string;
   description?: string;
   generator?: string;
+  image?: string;
+  pageUrl?: string;
 }
 
 const BaseHead: React.FC<IProps> = ({
   title,
   description = "Welcome to amirazmi.dev. Here can learn more about me and what I have been doing in the frontend development space.",
   generator,
+  image,
+  pageUrl = "https://amirazmi.dev",
 }) => {
   return (
     <>
@@ -22,6 +26,19 @@ const BaseHead: React.FC<IProps> = ({
       <title>{title}</title>
       <meta name="title" content={title} />
       {description && <meta name="description" content={description} />}
+
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content={pageUrl} />
+      <meta property="og:image" content={image} />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={image} />
     </>
   );
 };
