8cf0dd3cd82c6bf168e731ff47a5a0b4c7f2a6e8
Muhamad Amir <m.amirazmi@hotmail.com>
2023-12-04 18:25:12 +0800

    Add label to home

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,12 +6,16 @@ const Header: React.FC<IProps> = () => {
   return (
     <header className="w-full">
       <nav className="flex items-center max-w-6xl p-4 mx-auto">
-        <a href="/" className="flex gap-1">
+        <a href="/" aria-label="Homepage" className="flex gap-1">
           <span className="logo text-4xl font-semibold tracking-wider">AA</span>
           <span className="h-2 w-2 bg-primary rounded-full animate-bounce self-end mb-[6px]"></span>
         </a>
         <div className="ml-auto flex items-center gap-6">
-          <a href="/" className="text-muted-foreground hover:text-primary">
+          <a
+            href="/"
+            aria-label="Homepage"
+            className="text-muted-foreground hover:text-primary"
+          >
             <Home size={18} />
           </a>
           <a
