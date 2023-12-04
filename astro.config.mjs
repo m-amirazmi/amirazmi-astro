27d315954e9489ea3314e3e3de0db1aca75123ab
Muhamad Amir <m.amirazmi@hotmail.com>
2023-12-04 18:13:17 +0800

    add robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,19 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), react(), tailwind({ applyBaseStyles: false })],
+  site: "https://amirazmi.dev/",
+  integrations: [
+    mdx(),
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    robotsTxt(),
+  ],
 });
