3359fb1d3d5a854e2713802028ac386bea046e59
Muhamad Amir <muhamadamir.binazmi@etiqa.com.my>
2024-10-06 02:05:19 +0800

    Initiate new astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,11 @@
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
+// @ts-check
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
 
-import robotsTxt from "astro-robots-txt";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://amirazmi.dev/",
-  integrations: [
-    mdx(),
-    react(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    robotsTxt(),
-  ],
+	site: 'https://example.com',
+	integrations: [mdx(), sitemap()],
 });
